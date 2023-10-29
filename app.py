d6d3cc4d6c87a69212895a0697449fe0b6c99c46
Kevin H <main.share.us@gmail.com>
2023-10-29 02:22:14 -0700

    I'm stuck :( setting up cockroach

--- app.py
+++ app.py
@@ -1,16 +1,55 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy.orm
+# from cockroachdb.sqlalchemy import run_transaction
 
 app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+db = SQLAlchemy(app)
+sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
+
+
+### MODELS ###
+class Room(db.Model):
+    __tablename__ = 'rooms'
+    id = db.Column('room_id', db.Integer, primary_key=True)
+    status = db.Column(db.Integer)
+
+    def __init__(self, id):
+        self.id = id
+        self.status = 0
+
+
+class Player(db.Model):
+    __tablename__ = 'players'
+    id = db.Column('player_id', db.Intger, primary_key=True)
+
+    def __init__(self, id):
+        self.id = id
+
+
+# Junction table btw players and rooms (many to one)
+class PlayerRoom(db.Model):
+    __tablename__ = 'player_rooms'
+    player_id = db.Column('player_id', db.Integer, primary_key=True)
+    room_id = db.Column('room_id', db.Integer)
+
+    def __init__(self, player_id, room_id):
+        self.player_id = player_id
+        self.room_id = room_id
+
+
+##############
 
 @app.get("/register_new_device")
 def register_new_device():
     # Database lookup, find the smallest unique number
-    smallest_unique = 4
+    smallest = session.query(Player).order_by(Player.id.desc())
     
     # Add new player to the database (give new player a unique id)
     new_id = smallest_unique + 1
     
-    return str(new_id)
+    return str(smallest)
 
 @app.get("/create_room/<player_id>")
 def create_room(player_id):
